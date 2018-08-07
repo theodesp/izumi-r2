178e2faae57032febdad69ef496091775fec63dc
ratoshniuk <mratoshniuk@gmail.com>
2018-08-07 19:06:03 +0300

    fixed #37

--- logstage/logstage-api-logger/src/test/scala/com/github/pshirshov/izumi/logstage/api/routing/LoggingAsyncSinkTest.scala
+++ logstage/logstage-api-logger/src/test/scala/com/github/pshirshov/izumi/logstage/api/routing/LoggingAsyncSinkTest.scala
@@ -2,7 +2,7 @@ package com.github.pshirshov.izumi.logstage.api.routing
 
 import com.github.pshirshov.izumi.fundamentals.platform.build.ExposedTestScope
 import com.github.pshirshov.izumi.logstage.api.Log.CustomContext
-import com.github.pshirshov.izumi.logstage.api.config.LoggerConfig
+import com.github.pshirshov.izumi.logstage.api.config.LoggerPathConfig
 import com.github.pshirshov.izumi.logstage.api.logger.LogSink
 import com.github.pshirshov.izumi.logstage.api.rendering.{RenderingOptions, StringRenderingPolicy}
 import com.github.pshirshov.izumi.logstage.api.{IzLogger, Log, TestSink}
@@ -91,7 +91,7 @@ object LoggingAsyncSinkTest {
   }
 
   def mkRouter(sinks: LogSink*): ConfigurableLogRouter = {
-    val configService = new LogConfigServiceStaticImpl(Map.empty, LoggerConfig(Log.Level.Trace, sinks))
+    val configService = new LogConfigServiceImpl(Map.empty, LoggerPathConfig(Log.Level.Trace, sinks))
     val router = new ConfigurableLogRouter(configService)
     router
   }
